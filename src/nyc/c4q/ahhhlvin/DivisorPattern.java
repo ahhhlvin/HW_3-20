2f94c0ae08735dc4f0d4479542892f127779a250
ahhhlvin <alvkuang@gmail.com>
2015-03-18 22:06:40 -0400

    added divisor pattern art

--- src/nyc/c4q/ahhhlvin/DivisorPattern.java
+++ src/nyc/c4q/ahhhlvin/DivisorPattern.java
@@ -7,14 +7,34 @@ package nyc.c4q.ahhhlvin;
  */
 
 
-
+import java.util.Scanner;
 
 public class DivisorPattern {
     public static void main(String[] args) {
 
-        int n;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please a number: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
 
 
+                if ((i % j == 0) || (j % i == 0)) {
+                    System.out.print("@ ");
+                } else {
+                    System.out.print("  ");
+                }
 
+
+            }
+
+            System.out.println(" ");
+
+
+        }
     }
 }
