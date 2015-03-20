587c2a6944b75eb57ecba8346e7943cc2d7fba97
ahhhlvin <alvkuang@gmail.com>
2015-03-20 14:05:02 -0400

    verbcalculator updated

--- src/nyc/c4q/ahhhlvin/APrettyTitle.java
+++ src/nyc/c4q/ahhhlvin/APrettyTitle.java
@@ -25,7 +25,7 @@ public class APrettyTitle {
 
         Scanner input = new Scanner(title);
 
-        String sepWord;
+        String sepWord;   // variable
         String finalResult = ""; // initialize as empty
         String word;
 
