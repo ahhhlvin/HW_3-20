a0a99d06b44f3796e6f0844699d1345d54a0bf31
ahhhlvin <alvkuang@gmail.com>
2015-03-21 11:45:58 -0400

    updates

--- src/nyc/c4q/ahhhlvin/DivisorPattern.java
+++ src/nyc/c4q/ahhhlvin/DivisorPattern.java
@@ -19,9 +19,9 @@ public class DivisorPattern {
         System.out.println("Please enter a number: ");
         int n = input.nextInt();
 
-        for (int i = 1; i <= n; i++) {  // outer for loop starts each ROW
+        for (int i = 1; i <= n; i++) {  // OUTER for loop starts each ROW
 
-            for (int j = 1; j <= n; j++) {  // inner for loop goes through the COLUMNS for each row
+            for (int j = 1; j <= n; j++) {  // INNER for loop goes through the COLUMNS for each row
 
 
                 if ((i % j == 0) || (j % i == 0)) { // % is called 'modulo' which means divides evenly WITHOUT any remainders
